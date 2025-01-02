249c4f01812f6f059e279fac7901656c84a2ec5e
Darian Nwankwo <nwankwodarian@gmail.com>
2025-01-02 02:15:20 -0500

    added docstrings

--- lazy_struct.jl
+++ lazy_struct.jl
@@ -1,4 +1,4 @@
-"""
+@doc raw"""
 The `LazyStruct` table is used for functional-style lazy evaluation.  We set
 entries to a function that can produce the correct value; each such function is
 invoked the first time it is needed, and aftwerward we use a cached value.
@@ -21,25 +21,23 @@ end
 
 get_observations(ls::LazyStruct) = ls.y
 
-"""
+@doc raw"""
 When a property is set on the LazyStruct, we store it as a thunk. The thunk
 set is typically a function that returns the value of the property.
 """
-
 function Base.setproperty!(s :: LazyStruct, v :: Symbol, f::Function)
     thunks = getfield(s, :thunks)
     thunks[v] = f
     delete!(getfield(s, :values), v)
 end
 
-"""
+@doc raw"""
 When a property is accessed, we first check if it is in the values table. If
 so, we return the value.  If not, we check if it is in the thunks table.  If
 so, we call the thunk to get the value, store it in the values table, and
 return it.  If it is not in either table, we return the value of the property
 in the struct itself.
 """
-
 function Base.getproperty(s :: LazyStruct, v :: Symbol)
     values = getfield(s, :values)
     thunks = getfield(s, :thunks)
@@ -52,11 +50,10 @@ function Base.getproperty(s :: LazyStruct, v :: Symbol)
     getfield(s, v)
 end
 
-"""
+@doc raw"""
 We can also set values directly, without using a thunk.  This is useful for
 values that are not computed lazily.
 """
-
 function set(s :: LazyStruct, k :: Symbol, v)
     getfield(s, :values)[k] = v
 end
