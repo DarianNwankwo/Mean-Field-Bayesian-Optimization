912809964f950db2130edb84a05070120459e2f1
Darian Nwankwo <nwankwodarian@gmail.com>
2025-02-07 15:27:46 -0500

    added logic for arbitrary mean fields

--- experiments/run.jl
+++ experiments/run.jl
@@ -1,3 +1,50 @@
+using ArgParse
+
+
+@doc raw"""parse_command_line
+
+The parameters we are interested in changing are controlled by modifying the arguments passed to a CLI program.
+"""
+function parse_command_line(args)
+    parser = ArgParseSettings("Bayesian Optimization CLI")
+
+    @add_arg_table! parser begin
+        "--seed"
+            action = :store_arg
+            help = "Seed for random number generation"
+            default = 1906
+            arg_type = Int
+        "--starts"
+            action = :store_arg
+            help = "Number of random starts for solving the acquisition function (default: 64)"
+            default = 64
+            arg_type = Int
+        "--trials"
+            action = :store_arg
+            help = "Number of trials with a different initial start (default: 50)"
+            default = 50
+            arg_type = Int
+        "--budget"
+            action = :store_arg
+            help = "Maximum budget for bayesian optimization (default: 200)"
+            default = 200
+            arg_type = Int
+        "--function-name"
+            action = :store_arg
+            help = "Name of the synthetic function to optimize"
+            required = true
+        "--optimize"
+            action = :store_true
+            help = "If set, estimate optimal hyperparameters via MLE"
+    end
+
+    parsed_args = parse_args(args, parser)
+    return parsed_args
+end
+
+
+cli_args = parse_command_line(ARGS)
+
 include("../bayesian_optimization.jl")
 
 
---
+++ trends.jl
@@ -0,0 +1,29 @@
+struct PolynomialTrend
+    ϕ::PolynomialBasisFunction
+    coefficients::Vector{Float64}
+end
+
+
+# Want to evaluate the polynomial at some arbitrary location and use this mechanism
+# to adds arbitrary trends to a TestFunction
+function (pt::PolynomialTrend)(x::Vector{T}) where T <: Real
+    return dot(eval_basis(pt.ϕ, x), pt.coefficients)
+end
+
+function gradient(pt::PolynomialTrend, x::Vector{T}) where T <: Real
+    Px = eval_∇basis(pt.ϕ, x)
+    return Px * pt.coefficients
+end
+
+function +(testfn::TestFunction, pt::PolynomialTrend)
+    new_f = (x) -> testfn.f(x) + pt(x)
+    new_∇f = (x) -> testfn.∇f(x) + gradient(pt, x)  # Adding constant gradient
+    
+    return TestFunction(
+        testfn.dim,
+        testfn.bounds,
+        testfn.xopt,
+        new_f,
+        new_∇f
+    )
+end
