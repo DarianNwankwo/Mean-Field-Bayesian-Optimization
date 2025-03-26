5a05916851e13f0ee39c03f193d6eb665ef75cc3
Darian Nwankwo <nwankwodarian@gmail.com>
2025-03-26 15:06:09 -0400

    dependency installation script

---
+++ install.jl
@@ -0,0 +1,16 @@
+using Pkg
+
+Pkg.add([
+    "Plots",
+    "Sobol",
+    "Distributions",
+    "Optim",
+    "ForwardDiff",
+    "Statistics",
+    "Roots",
+    "FastGaussQuadrature",
+    "IterTools",
+    "Tables",
+    "CSV",
+    "DataFrames"
+])
